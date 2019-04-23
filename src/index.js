b245f015c2509f590e2dcabad2258c147cfffd0b
mhsmans <mh.smans@gmail.com>
2019-04-23 13:02:45 +0200

    Changed settings for working with heroku.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-const app = require('./server');
-const config = require('./config/env/env');
+const app = require("./server");
+const config = require("./config/env/env");
 
-app.listen(config.env.webPort, function () {
-    console.log('Server listening on port ' + app.get('port'));
-});
+const MongoClient = require("mongodb").MongoClient;
+const CONNECTION_URL =
+  "mongodb+srv://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "concertiodb-dndfi.gcp.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = process.env.DB_DATABASE;
+
+// const database, collection;
+
+if (process.env == "production") {
+  app.listen(config.env.webPort, () => {
+    MongoClient.connect(
+      CONNECTION_URL,
+      { useNewUrlParser: true },
+      (error, client) => {
+        if (error) {
+          throw error;
+        }
+        // database = client.db(DATABASE_NAME);
+        console.log(
+          "Connected to `" + DATABASE_NAME + "` on port " + app.get("port")
+        );
+      }
+    );
+  });
+} else {
+  app.listen(config.env.webPort, function() {
+    console.log("Server listening on port " + app.get("port"));
+  });
+}
