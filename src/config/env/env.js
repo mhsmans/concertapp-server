447c59dd1fe8d9ee6525f19e4de7a875799c8366
mhsmans <mh.smans@gmail.com>
2019-07-09 13:51:54 +0200

    Atlas fix

--- src/config/env/env.js
+++ src/config/env/env.js
@@ -1,20 +1,16 @@
 var env = {
     webPort: process.env.PORT || 3000,
     dbHost: process.env.DB_HOST || 'local',
-    dbPort: process.env.DB_PORT || '',
     dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
     dbDatabase: process.env.DB_DATABASE || 'concertapp'
 }
 
 var dburl = process.env.NODE_ENV === 'production' ?
-    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
+    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost :
     'mongodb://localhost/' + env.dbDatabase
 
 module.exports = {
     env: env,
     dburl: dburl
 };
-
-
-
