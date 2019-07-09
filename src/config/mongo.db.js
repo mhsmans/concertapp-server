7ce161bde670ce777f6669844d69c9863984a1b8
mhsmans <mh.smans@gmail.com>
2019-07-09 15:30:33 +0200

    Trying settings

--- src/config/mongo.db.js
+++ src/config/mongo.db.js
@@ -1,22 +1,11 @@
 const mongoose = require("mongoose");
 const config = require("./env/env");
-const MongoClient = require('mongodb').MongoClient;
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
-// mongoose.connect(config.dburl, { useNewUrlParser: true });
-// mongoose.connection
-//   .once("open", () => console.log("Connected to Mongo on " + config.dburl))
-//   .on("error", error => {
-//     console.warn("Warning", error.toString());
-// });
-
-MongoClient.connect(config.dburl, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-   // const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
+mongoose.connect(config.dburl, { useNewUrlParser: true });
+mongoose.connection
+  .once("open", () => console.log("Connected to Mongo on " + config.dburl))
+  .on("error", error => {
+    console.warn("Warning", error.toString());
 });
